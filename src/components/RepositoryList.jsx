cedf11f44a931ef5835e90221e439c7478e405d8
NehamaMandelbaum <nehama1836@gmail.com>
2021-08-11 14:38:58 -0300

    erro no css

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+import '../styles/repositories.scss';
+
+const repository = {
+    name: 'unform', 
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
+export function RepositoryList(){
+    return (
+        <section className ='repositoryList'>
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+            </ul>
+        </section>
+    )
+}
